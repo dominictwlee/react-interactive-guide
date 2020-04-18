7b1fcf720e12efe1edcff92393f4ed34d1c0a80b
Dominic Lee <dominictwlee@gmail.com>
2020-04-18 13:07:32 +0100

    chore: setup ci with semantic-release

---
+++ release.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  plugins: [
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        preset: 'angular',
+        releaseRules: [
+          { type: 'refactor', release: 'patch' },
+          { type: 'docs', scope: 'README', release: 'patch' },
+        ],
+      },
+    ],
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    '@semantic-release/npm',
+    [
+      '@semantic-release/git',
+      {
+        assets: ['package.json', 'CHANGELOG.md'],
+        message:
+          // eslint-disable-next-line no-template-curly-in-string
+          'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
+    '@semantic-release/github',
+  ],
+};
