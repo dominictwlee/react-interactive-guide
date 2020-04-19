21086451317504cccaae3abcdf93344c020a53a2
Dominic Lee <dominictwlee@gmail.com>
2020-04-19 17:35:19 +0100

    chore: add refactoring seciton to release notes

--- release.config.js
+++ release.config.js
@@ -7,7 +7,24 @@ module.exports = {
         releaseRules: [{ type: 'refactor', release: 'patch' }],
       },
     ],
-    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/release-notes-generator',
+      {
+        preset: 'angular',
+        presetConfig: {
+          types: [
+            { type: 'feat', section: 'Features' },
+            { type: 'fix', section: 'Bug Fixes' },
+            { type: 'chore', hidden: true },
+            { type: 'docs', hidden: true },
+            { type: 'style', hidden: true },
+            { type: 'refactor', section: 'Code Refactoring' },
+            { type: 'perf', hidden: true },
+            { type: 'test', hidden: true },
+          ],
+        },
+      },
+    ],
     '@semantic-release/changelog',
     '@semantic-release/npm',
     [
