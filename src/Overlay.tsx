0eff5948682143c6c828e4aa81a899af80e7b920
Dominic Lee <dominictwlee@gmail.com>
2020-04-27 21:15:14 +0100

    fix: position fixed for overlay

--- src/Overlay.tsx
+++ src/Overlay.tsx
@@ -7,12 +7,12 @@ type OverlayProps = {
 
 const Overlay = animated(
   styled.div<OverlayProps>(({ isIdle }) => ({
+    position: 'fixed',
     bottom: 0,
     left: 0,
-    overflow: 'hidden',
-    position: 'absolute',
     right: 0,
     top: 0,
+    overflow: 'hidden',
     visibility: isIdle ? 'hidden' : 'visible',
     zIndex: isIdle ? -1 : 2300,
     backgroundColor: 'rgba(26, 29, 52, 0.5)',
