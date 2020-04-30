4bc054fb8765421a0b7011aafcd6c50a3cf957d4
Dominic Lee <dominictwlee@gmail.com>
2020-04-30 14:49:42 +0100

    feat: export tourguide style props

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,9 @@ export { default as useGuide } from './useGuide';
 export { default as Tourguide } from './Tourguide';
 export type { TourguideProps } from './Tourguide';
 export type { UseTourguideProps } from './useTourguide';
+export type {
+  StyleConfig as TourguideStyleConfig,
+  GlobalStyles as TourguideGlobalStyles,
+  PositionStyles as TourguidePositionStyles,
+} from './types';
 export { default as TourguideProvider } from './TourguideProvider';
